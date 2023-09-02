05e34ccd0198cffc1b0b7147af74b9722492515b
mdnobin96670 <farhanahmednobin@gmail.com>
2023-09-03 01:12:04 +0600

    adding band name genarator project

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+cityName = input("Enter your city name where you grow up in: ");
+petName = input("Enter your favorite pet name: ");
+bandName = (cityName+" "+petName);
+print("Your band name:", bandName);
+
