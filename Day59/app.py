fede389f614433256609878710d9ca2ee3b9bb73
mdnobin96670 <farhanahmednobin@gmail.com>
2023-11-03 23:32:26 +0600

    Day 59

---
+++ Day59/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/post")
+def post():
+    return render_template("/post.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
