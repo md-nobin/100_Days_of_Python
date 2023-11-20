5e8b9f7c3c2d6b1df32bc744bf934f3946d7f245
mdnobin96670 <farhanahmednobin@gmail.com>
2023-11-20 22:15:54 +0600

    Day 52

--- Day52/main.py
+++ Day52/main.py
@@ -2,48 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-SIMILAR_ACCOUNT = "coding_dev_"
-USERNAME = "sagar_shahriar_nobin"
-PASSWORD = "SIP9670"
+driver = webdriver.Chrome()
+driver.get("https://www.instagram.com/accounts/login/")
 
+# Login into account
+time.sleep(10)
+driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label').send_keys(os.getenv('USERID'))
+driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label').send_keys(os.getenv('PASSWORD'))
+driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').click()
 
-class InstaFollower:
-    def __init__(self):
-        self.driver = webdriver.Chrome()
+# Get the target account
+time.sleep(20)
+driver.get(f"https://www.instagram.com/{os.getenv('SIMILAR_ACCOUNT')}")
 
-    def login(self):
-        self.driver.get("https://www.instagram.com/accounts/login/")
-        user_name = self.driver.find_element(By.XPATH, "//*[@id=\"loginForm\"]/div/div[1]/div/label")
-        user_name.click()
-        user_name.send_keys(USERNAME)
+# Follow account
+time.sleep(10)
+follower_accounts = driver.find_element(By.CSS_SELECTOR, 'div._aano')
 
-        time.sleep(10)
-        password = self.driver.find_element(By.XPATH, "//*[@id=\"loginForm\"]/div/div[2]/div/label")
-        password.click()
-        password.send_keys(PASSWORD)
+while True:
+    pass
 
-        time.sleep(3)
-        login = self.driver.find_element(By.XPATH, "//*[@id=\"loginForm\"]/div/div[3]/button")
-        login.click()
-
-    def find_followers(self):
-        time.sleep(30)
-        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
-        time.sleep(30)
-        followers = self.driver.find_element(By.LINK_TEXT, "followers")
-
-        while True:
-            pass
-
-    def follow(self):
-        pass
-
-    # while True:
-    #     pass
-
-
-bot = InstaFollower()
-bot.login()
-bot.find_followers()
-bot.follow()
+# account locked for repeatedly login , wait for a day, if account doesn't unlocked then use another account.
