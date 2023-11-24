6426e0f8b11a49bb24d8d14003a40d4560bdcfa6
mdnobin96670 <farhanahmednobin@gmail.com>
2023-11-24 23:48:15 +0600

    Compleat day 64

--- Day64/Top-movies/app.py
+++ Day64/Top-movies/app.py
@@ -5,7 +5,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+tmdb_url = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1"
+tmdb_api_key = os.getenv("api_key")
+movie_db_info_url = "https://api.themoviedb.org/3/movie/"
+movie_db_image_url = "https://image.tmdb.org/t/p/original/"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -36,36 +43,10 @@ class RateMovieForm(FlaskForm):
     review = StringField("Your Review")
     submit = SubmitField("Done")
 
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
 
-#
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.commit()
-
-#
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-
-
-# with app.app_context():
-#     db.session.add(second_movie)
-#     db.session.commit()
+class AddMovieForm(FlaskForm):
+    title = StringField("Add Movie", validators=[DataRequired()])
+    submit = SubmitField("Add")
 
 
 @app.route("/")
@@ -87,6 +68,46 @@ def rate_movie():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("edit.html", movie=movie, form=form)
+
+
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/add_movie", methods=['GET', 'POST'])
+def add_new_movie():
+    form = AddMovieForm()
+
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(tmdb_url, params={"api_key":tmdb_api_key, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{movie_db_info_url}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": tmdb_api_key, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{movie_db_image_url}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("rate_movie", id=new_movie.id))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
