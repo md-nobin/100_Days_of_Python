de54b4f96fb6585de97f893f74a9be1cff0151e0
mdnobin96670 <farhanahmednobin@gmail.com>
2023-12-02 04:13:02 +0600

    Compleat day 68

--- Day68/Secure Login/app.py
+++ Day68/Secure Login/app.py
@@ -10,60 +10,87 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy()
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
 
- 
+
 with app.app_context():
     db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-
         hash_and_salted_password = generate_password_hash(
-            request.form.get("password"),
-            method="pbkdf2:sha256",
+            request.form.get('password'),
+            method='pbkdf2:sha256',
             salt_length=8
         )
         new_user = User(
-            email=request.form.get("email"),
-            name=request.form.get("name"),
-            password=hash_and_salted_password
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=hash_and_salted_password,
         )
+
         db.session.add(new_user)
         db.session.commit()
 
-        return render_template("secrets.html", name=request.form.get("name"))
+        login_user(new_user)
+
+        # Can redirect() and get name from the current_user
+        return redirect(url_for("secrets"))
+
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("secrets"))
+
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("home"))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory("static", path="files/cheat_sheet.pdf")
 
---
+++ Day68/readme.md
@@ -0,0 +1,10 @@
+- flask_login
+- Authentication
+- hashing algorithm
+- Encryption
+- Deception
+
+Today's learning covered Flask_Login integration for user sessions.
+I explored advanced password hashing for secure credentials and highlighted encryption's role in data protection.
+Additionally, deception mechanisms were discussed to fortify defenses against potential threats,
+ensuring a comprehensive grasp of web security.
